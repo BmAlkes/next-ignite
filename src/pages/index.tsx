61d7b8d6da1e86dc7d6f6c984cf2fee2a696a797
bmalkes <bmalkes@gmail.com>
2023-05-14 14:53:01 +0300

    feat:add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { Inter } from "next/font/google";
+import { styled } from "./styles";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const Button = styled("button", {
+  backgroundColor: "$gray100",
+  margin: "0px",
+  border: "0px",
+  padding: "1rem 2rem",
+  borderRadius: 4,
+  color: "white",
+});
 export default function Home() {
   return (
     <>
       <h1>Hello world</h1>
+      <Button>Hello</Button>
     </>
   );
 }
