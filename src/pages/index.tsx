7321762f000d0f3de77e7262e79f17100011b436
bmalkes <bmalkes@gmail.com>
2023-05-15 10:23:35 +0300

    feat:add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,27 @@
-import { Inter } from "next/font/google";
-import { styled } from "./styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "./styles/pages/home";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const Button = styled("button", {
-  backgroundColor: "$gray100",
-  margin: "0px",
-  border: "0px",
-  padding: "1rem 2rem",
-  borderRadius: 4,
-  color: "white",
-});
+import camiseta1 from "../pages/assets/Camisa-Maratona 1.png";
+import camiseta2 from "../pages/assets/IgniteLab-T-shirt 1.png";
+import camiseta3 from "../pages/assets/Igniter-abord-2-t-shirt 1.png";
+import camiseta4 from "../pages/assets/Variant6.png";
 export default function Home() {
   return (
-    <>
-      <h1>Hello world</h1>
-      <Button>Hello</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 39.99</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 59.99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
