cb10cebd79c29ec5dfcfdda150252bbf2da7c756
bmalkes <bmalkes@gmail.com>
2023-05-15 10:40:31 +0300

    feat:add keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,46 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "./styles/pages/home";
+import { useKeenSlider } from "keen-slider/react";
 
 import camiseta1 from "../pages/assets/Camisa-Maratona 1.png";
 import camiseta2 from "../pages/assets/IgniteLab-T-shirt 1.png";
 import camiseta3 from "../pages/assets/Igniter-abord-2-t-shirt 1.png";
 import camiseta4 from "../pages/assets/Variant6.png";
+
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta X</strong>
           <span>$ 39.99</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 59.99</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 59.99</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta X</strong>
           <span>$ 59.99</span>
--- src/pages/styles/pages/home.ts
+++ src/pages/styles/pages/home.ts
@@ -2,9 +2,9 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  gap: "3rem",
+  //   gap: "3rem",
   width: "100%",
-  maxWidth: "calc(100vw - ((100vw - 1180)/ 2))",
+  maxWidth: "calc(100vw - ((100vw - 1180px)/ 2))",
   marginLeft: "auto",
   minHeight: 656,
 });
