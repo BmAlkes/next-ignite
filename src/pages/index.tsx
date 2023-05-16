c8ee41f66e0f39e7f6879ef033931c43109ebe14
bmalkes <bmalkes@gmail.com>
2023-05-16 09:09:52 +0300

    feat:add product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "./styles/pages/home";
 import { useKeenSlider } from "keen-slider/react";
+import Link from "next/link";
 
 import "keen-slider/keen-slider.min.css";
 import { stripe } from "./lib/stripe";
@@ -28,13 +29,15 @@ export default function Home({ products }: HomeProps) {
       {products.map((product) => {
         console.log(product.imageUrl);
         return (
-          <Product className="keen-slider__slide" key={product.id}>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>$ {product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>$ {product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,28 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetail,
+} from "../styles/pages/product";
 
 const Product = () => {
   const { query } = useRouter();
-  return <div>Product : {JSON.stringify(query)}</div>;
+  return (
+    <ProductContainer>
+      <ImageContainer>w</ImageContainer>
+      <ProductDetail>
+        <h1>Camiset x</h1>
+        <span>$ 79,9</span>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+          perferendis hic distinctio molestias recusandae atque impedit nulla
+          qui ex consequuntur, odio minus, alias, nobis animi delectus ipsum aut
+          porro non?
+        </p>
+        <button>Buy Now</button>
+      </ProductDetail>
+    </ProductContainer>
+  );
 };
 
 export default Product;
